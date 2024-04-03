f08ba9cc3f8809e32094a935b41ebae5d9282b1e
orlowski11 <pawel.orlowski@studenci.collegiumwitelona.pl>
2024-04-03 21:58:24 +0200

    mockito version upgrade & dynamicColor false

--- app/src/main/java/com/example/lolify_android/ui/theme/Theme.kt
+++ app/src/main/java/com/example/lolify_android/ui/theme/Theme.kt
@@ -53,7 +53,7 @@ onSurface = Color(0xFF1C1B1F),
 fun LolifyandroidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
